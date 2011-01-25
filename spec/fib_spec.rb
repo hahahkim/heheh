f08c3fa66877da56f6567fa0e91e8dbd05f5000f
Kim Eunsoo <hahah@kaist.ac.kr>
2011-01-25 20:50:29 +0900

    test n=2

--- spec/fib_spec.rb
+++ spec/fib_spec.rb
@@ -1,7 +1,11 @@
 require 'fib'
 
 describe Fib do
-	it 'should return 1 withe argument 1' do
+	it 'should return 1 with argument 1' do
+		fib = Fib.new
+		fib.get(1).should == 1
+	end
+	it 'should return 1 with argument 2' do
 		fib = Fib.new
 		fib.get(1).should == 1
 	end
