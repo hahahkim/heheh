91270dfc2789c955814876d513cf6e3e31a59774
Kim Eunsoo <hahah@kaist.ac.kr>
2011-01-25 21:42:31 +0900

    timer final

--- lib/timer.rb
+++ lib/timer.rb
@@ -1,3 +1,6 @@
 class Timer
+	def date_string
+		Time.now.strftime('%Y/%m/%d')
+	end
 end
 
--- spec/timer_spec.rb
+++ spec/timer_spec.rb
@@ -4,7 +4,10 @@ describe Timer do
 	before do
 		@timer=Timer.new
 	end
-	it 'test' do
+	it 'should return the today date when 2002/01/01' do
+		#change computer time
+		Time.stub!(:now).and_return(Time.new(2002,1,1))
+		@timer.date_string.should == '2002/01/01'
 	end
 end
 
